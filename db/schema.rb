1ea6368b2a4820a26504339486c9428e6fc3fbc0
faisalalsudani <f4desiign@gmail.com>
2017-11-10 20:11:28 +0100

    new calculations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110182513) do
+ActiveRecord::Schema.define(version: 20171110185721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171110182513) do
     t.string "phone"
     t.string "bsn_num"
     t.integer "praktijk"
+    t.integer "theorie"
   end
 
   create_table "users", force: :cascade do |t|
